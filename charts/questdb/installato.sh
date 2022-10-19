c87122e2b48bb8d8b2de352fc2c1e3848533f700
Andrea Lai <alai.arpas@gmail.com>
2022-10-19 07:28:54 +0200

    helm questdb andrea

---
+++ charts/questdb/installato.sh
@@ -0,0 +1,10 @@
+helm install questdb questdb/questdb -f values_andrea.yaml --namespace poa-backend 
+
+# NAME: questdb
+# LAST DEPLOYED: Tue Oct 18 11:05:31 2022
+# NAMESPACE: poa-backend
+# STATUS: deployed
+# REVISION: 1
+# NOTES:
+# 1. Get the application URL by running these commands:
+https://questdb.alai.one/
